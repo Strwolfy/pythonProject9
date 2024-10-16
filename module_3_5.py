81b2ac3e75779b4ae22d601c99ca1fb183c14ea6
Strwolfy <strwolfy@gmail.com>
2024-10-16 16:01:38 +0300

    fix module_3_5

--- module_3_5.py
+++ module_3_5.py
@@ -1,18 +1,18 @@
 def get_multiplied_digits(number):
-    # Преобразуем число в строку
-    str_number = str(number)
+    str_number = str(number)  # Преобразуем число в строку
+    first = int(str_number[0])  # Получаем первую цифру числа
 
-    # Извлекаем первую цифру и преобразуем её обратно в число
-    first = int(str_number[0])
+    # Если первая цифра 0, то пропускаем её
+    if first == 0:
+        return get_multiplied_digits(int(str_number[1:])) if len(str_number) > 1 else 1
 
-    # Если в числе осталась только одна цифра, возвращаем её
-    if len(str_number) == 1:
-        return first
+    # Если длина числа больше 1, рекурсивно вызываем функцию для оставшейся части
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first  # Возвращаем первую цифру, если больше цифр нет
 
-    # Рекурсивный вызов функции для оставшихся цифр числа
-    return first * get_multiplied_digits(int(str_number[1:]))
 
-
-# Пример вызова функции
-result = get_multiplied_digits(40203)
-print(result)  # Ожидаемый результат: 24
+# Пример использования
+result = get_multiplied_digits(42220)
+print(result)
